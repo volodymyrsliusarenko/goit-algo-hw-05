0bd28bc8a2e128fb6d843afb07e36e20ad47581b
Volodymyr Sliusarenko <volodymyr.slusarenko@gmail.com>
2024-03-23 19:22:12 +0200

    Task 1

---
+++ Task_1.py
@@ -0,0 +1,22 @@
+def caching_fibonacci():
+    cache = {}
+
+    def fibonacci(n):
+        if n <= 0:
+            return 0
+        if n in cache:
+            return cache[n]
+        if n == 1:
+            return 1
+
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+
+    return fibonacci
+
+# Отримуємо функцію fibonacci
+fib = caching_fibonacci()
+
+# Використовуємо функцію fibonacci для обчислення чисел Фібоначчі
+print(fib(10))  # Виведе 55
+print(fib(15))  # Виведе 610
